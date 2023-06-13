6c5a8576aae571afee3719c48105dfcdfbfb665a
James Hall <hi@jameshall.xyz>
2023-06-12 17:48:44 -0700

    Added more levels

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const context = canvas.getContext("2d");
 const pugDimensions = { width: 353 * 1.2, height: 325 * 1.2 };
 
 
-const levels = {
+const positiveLevels = {
+  0: "Assistant",
   5: "Sr Assistant",
   10: "Jr Honoror",
   15: "Master Honoror",
@@ -24,6 +25,28 @@ const levels = {
   10500: "OverLord",
   20500: "King",
   30500: "Anunnaki"
+}
+
+const negativeLevels = {
+  5: "Minor Failer",
+  10: "Amateur Failer",
+  15: "Master Failer",
+  35: "S Tier Failer",
+  65: "Junior Disgrace",
+  105: "Disgrace",
+  150: "Senior Disgrace",
+  250: "Rascal",
+  450: "Pariah",
+  650: "Outcast",
+  1000: "Senior Outcast",
+  1500: "Unemployed",
+  2500: "Beggar",
+  3500: "Criminal",
+  4500: "Wanted Criminal",
+  10500: "Exiled",
+  20500: "Junior Evil Mastermind",
+  30500: "Evil Mastermind",
+  40500: "Senior Partner in the Firm of Evil Mastermind & Sons, Inc.",
 }
 
 const startTime = Date.now();
@@ -107,20 +130,38 @@ function loopDraw() {
 
   secondsCount.innerText = newTime;
 
-  if(levels[newTime]) {
-    level.innerText = levels[newTime]
+
+  if (newTime > 0) { // if the time is positive, use the positiveLevels
+    console.log('positive')
+    if (positiveLevels[newTime]) {
+    level.innerText = positiveLevels[newTime];
+    }
+  } else { // the time is negative, so use the negativeLevels
+    const levelNumbers = Object.keys(negativeLevels);
+
+    // find the highest level that is less than the current time
+    const currentRank = levelNumbers.reduce((highest, current) => {
+      const numOfSeconds = Math.abs(newTime); // convert to positive number
+      if (numOfSeconds >= current && current > highest) {
+        return parseInt(current)
+      } else { return parseInt(highest) }
+    }, 0);
+  
+    level.innerText = negativeLevels[currentRank];
   }
 
   requestAnimationFrame(loopDraw);
 }
 
+// on blur, record the time
 window.addEventListener('blur', (e) => {
   blurStart = Date.now();
 })
 
+// on window focus, record the time spent blurred and add it to the totalBlurredTime
 window.addEventListener('focus', (e) => {
   let blurredTime = Math.floor((Date.now() - blurStart) / 1000);
-  totalBlurredTime += blurredTime;
+  totalBlurredTime += blurredTime * 2; //this has to be doubled to counteract the time that passed while blurred
 })
 
 function startLooping() {
